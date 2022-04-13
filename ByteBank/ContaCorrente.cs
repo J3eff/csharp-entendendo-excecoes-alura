dc7ddf80ed8b52b2a0f3a56aac11583ff4666eeb
J3eff <jeffersonvideo125@gmail.com>
2022-04-13 17:03:46 -0300

    first commit

---
+++ ByteBank/ContaCorrente.cs
@@ -0,0 +1,75 @@
+﻿
+namespace _07_ByteBank
+{
+    public class ContaCorrente
+    {
+        public Cliente Titular { get; set; }
+
+        public static int TotalDeContasCriadas { get; private set; }      
+
+        private int _agencia;
+        public int Agencia 
+        {
+            get
+            {
+                return _agencia;
+            }
+            set
+            {
+                if (value <= 0)
+                    return;
+
+                _agencia = value;
+            } 
+        }
+
+        public int Numero { get; set; }
+
+        private double _saldo = 100;
+        public double Saldo
+        {
+            get { return _saldo; }
+
+            set
+            {
+                if (value < 0)
+                    return; //Para de executar o método, e devolve o fluxo do programa.
+
+                _saldo = value;
+            }
+        }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+            TotalDeContasCriadas++;
+        }
+
+
+        public bool Sacar(double valor)
+        {
+            if (_saldo < valor)
+                return false;
+
+            _saldo -= valor;
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente constaDestino)
+        {
+            if (_saldo < valor)
+                return false;
+
+            _saldo -= valor;
+            constaDestino.Depositar(valor);
+            return true;
+        }
+    }
+}
+
