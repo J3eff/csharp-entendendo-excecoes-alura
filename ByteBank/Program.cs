3811ee73b8ab1d68de7777fb10e756de5d824563
J3eff <jeffersonvideo125@gmail.com>
2022-04-13 17:49:03 -0300

    Lidando com erros

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -10,11 +10,61 @@ namespace _07_ByteBank
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(7480, 874150);
+            int resultado  = Metodo();
 
-            Console.WriteLine(ContaCorrente.TaxaOperacao);
+            if (resultado == -2) 
+                Console.WriteLine("Ocorreu um erro!");
 
             Console.ReadLine();
         }
+
+        /// Teste com a cadeia de chamada:
+        /// Metodo -> TestaDivisao -> Dividir
+        private static int Metodo()
+        {
+            int resultadoDivisao = TestaDivisao(0);
+
+            if (resultadoDivisao == -2) return -2;
+
+            int resultado2 = TestaDivisao(20);
+
+            if(resultado2 == -2) return -2;
+
+            return 0;
+        }
+
+        private static int TestaDivisao(int divisor)
+        {
+            int resultado = Dividir(10, divisor);
+
+            if (resultado == -2) return -2;
+            
+            if (resultado == -1)
+            {
+                Console.WriteLine("Não é possivel fazer divisão por 0");
+            } 
+            else
+            {
+                Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
+            }
+
+            return 0;
+        }
+
+        private static int Dividir(int numero, int divisor)
+        {
+            if(divisor == 0)
+            {
+                return -1;
+            }
+
+            if(divisor > numero)
+            {
+                return -2;
+            }
+
+            return numero / divisor;
+        }
+
     }
 }
